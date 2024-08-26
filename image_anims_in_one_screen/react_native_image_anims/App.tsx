eb9343617a49530cdeb35ded9a926d7f08245265
nateshmbhat <nateshmbhat1@gmail.com>
2024-08-26 13:29:52 +0530

    uses latest reanimated v3 for bulk image animations app

--- image_anims_in_one_screen/react_native_image_anims/App.tsx
+++ image_anims_in_one_screen/react_native_image_anims/App.tsx
@@ -1,12 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React, {Component, useRef, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -15,11 +7,16 @@ import {
   StatusBar,
   Image,
   Dimensions,
-  Animated,
-  Easing,
 } from 'react-native';
-
 import FastImage from 'react-native-fast-image';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withRepeat,
+  Easing,
+  interpolate,
+} from 'react-native-reanimated';
 
 const AnimatedFastImage = Animated.createAnimatedComponent(FastImage);
 
@@ -51,7 +48,7 @@ function getImage(num) {
 }
 
 let data = Array(200);
-for (var i = 0; i < data.length; i++) {
+for (let i = 0; i < data.length; i++) {
   data[i] = {
     key: String(i),
     src: getImage(i),
@@ -65,72 +62,16 @@ const App = () => {
       <SafeAreaView>
         <FlatList
           data={data}
-          // renderItem={({item}) => (
-          //   <View style={{flex: 1, flexDirection: 'column', margin: 1}}>
-          //     <Image
-          //       style={styles.imageThumbnail}
-          //       source={item.src}
-          //       resizeMode="cover"
-          //     />
-          //   </View>
-          // )}
           renderItem={({item}) => {
             let index = item.key % 3;
-            let spinValue = new Animated.Value(0);
-            if (index == 0) {
-              let spin = spinValue.interpolate({
-                inputRange: [0, 360],
-                outputRange: ['0deg', '360deg'],
-              });
-              let transform = [{rotate: spin}];
-              return (
-                <CustomRow
-                  index={item.key}
-                  useNativeDriver={true}
-                  transform={transform}
-                  spinValue={spinValue}>
-                  {' '}
-                </CustomRow>
-              );
+            if (index === 0) {
+              return <RotatingImage index={item.key} />;
             }
-            if (index == 1) {
-              let spin = spinValue.interpolate({
-                inputRange: [0, 360],
-                outputRange: [0.0, 1.0],
-              });
-              let transform = [{scale: spin}];
-              return (
-                <CustomRow
-                  index={item.key}
-                  useNativeDriver={true}
-                  transform={transform}
-                  spinValue={spinValue}>
-                  {' '}
-                </CustomRow>
-              );
+            if (index === 1) {
+              return <ScalingImage index={item.key} />;
             }
-            if (index == 2) {
-              return (
-                <View
-                  style={{
-                    flex: 1,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}>
-                  <FadeInView
-                    style={{
-                      height: Dimensions.get('window').width / 10,
-                      width: Dimensions.get('window').width / 10,
-                      backgroundColor: 'powderblue',
-                    }}>
-                    <Image
-                      style={styles.imageThumbnail}
-                      source={item.src}
-                      resizeMode="cover"
-                    />
-                  </FadeInView>
-                </View>
-              );
+            if (index === 2) {
+              return <FadingImage source={item.src} />;
             }
           }}
           numColumns={10}
@@ -141,74 +82,91 @@ const App = () => {
   );
 };
 
-const FadeInView = props => {
-  const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
+const RotatingImage = ({index}) => {
+  const rotateValue = useSharedValue(0);
 
-  React.useEffect(() => {
-    Animated.loop(
-      Animated.timing(fadeAnim, {
-        toValue: 1,
+  useEffect(() => {
+    rotateValue.value = withRepeat(
+      withTiming(360, {
         duration: 5000,
         easing: Easing.linear,
-        useNativeDriver: true,
       }),
-    ).start();
-  }, []);
+      -1,
+    );
+  }, [rotateValue]);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const rotate = `${rotateValue.value}deg`;
+    return {transform: [{rotate}]};
+  });
 
   return (
-    <Animated.View // Special animatable View
-      style={{
-        ...props.style,
-        opacity: fadeAnim, // Bind opacity to animated value
-      }}>
-      {props.children}
-    </Animated.View>
+    <AnimatedFastImage
+      style={[styles.imageThumbnail, animatedStyle]}
+      source={getImage(index)}
+      resizeMode={'cover'}
+    />
   );
 };
 
-class CustomRow extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      index: props.index,
-      //spin: props.spin,
-      spinValue: props.spinValue,
-      transform: props.transform,
-      useNativeDriver: props.useNativeDriver,
-    };
-  }
+const ScalingImage = ({index}) => {
+  const scaleValue = useSharedValue(0);
 
-  render() {
-    return (
-      <AnimatedFastImage
-        style={{
-          transform: this.state.transform,
-          height: Dimensions.get('window').width / 10,
-          width: Dimensions.get('window').width / 10,
-        }}
-        source={getImage(this.state.index)}
-        resizeMode={'cover'}
-        fadeDuration={0}
-      />
-    );
-  }
-
-  componentDidMount() {
-    this.animation = Animated.loop(
-      Animated.timing(this.state.spinValue, {
-        toValue: 360,
+  useEffect(() => {
+    scaleValue.value = withRepeat(
+      withTiming(1, {
         duration: 5000,
         easing: Easing.linear,
-        useNativeDriver: this.state.useNativeDriver,
       }),
-    ).start();
-  }
+      -1,
+      true,
+    );
+  }, [scaleValue]);
 
-  componentWillUnmount() {
-    this.state.spinValue.stopAnimation();
-    this.animation;
-  }
-}
+  const animatedStyle = useAnimatedStyle(() => {
+    const scale = interpolate(scaleValue.value, [0, 1], [0.5, 1]);
+    return {transform: [{scale}]};
+  });
+
+  return (
+    <AnimatedFastImage
+      style={[styles.imageThumbnail, animatedStyle]}
+      source={getImage(index)}
+      resizeMode={'cover'}
+    />
+  );
+};
+
+const FadingImage = ({source}) => {
+  const opacityValue = useSharedValue(0);
+
+  useEffect(() => {
+    opacityValue.value = withRepeat(
+      withTiming(1, {
+        duration: 5000,
+        easing: Easing.linear,
+      }),
+      -1,
+      true,
+    );
+  }, [opacityValue]);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: opacityValue.value,
+  }));
+
+  return (
+    <View style={styles.centeredContainer}>
+      <Animated.View style={[styles.imageThumbnail, animatedStyle]}>
+        <Image
+          style={styles.imageThumbnail}
+          source={source}
+          resizeMode="cover"
+        />
+      </Animated.View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   imageThumbnail: {
@@ -216,6 +174,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: Dimensions.get('window').width / 10,
     height: Dimensions.get('window').width / 10,
+  },
+  centeredContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
